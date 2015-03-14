624bd7508fc379fa3d209ab4f3a89d6c4e0d916b
steven-hl <steven.hl.0901@gmail.com>
2015-03-15 03:36:36 +0800

    add some config

--- config.py
+++ config.py
@@ -5,9 +5,17 @@ token = 'ilovemovie'
 appid = 'wxffaceb82ccad873e'
 appsecret = 'e168947a5cf61520d2477535c4f7322d'
 
+# crawler config
+douban_url = 'http://movie.douban.com/nowplaying/shanghai/'
+
 # server config
 server_host = '0.0.0.0'
 server_port = 8080
+
+# redis config
+redis_host = '127.0.0.1'
+redis_port = 6379
+redis_db = 1
 
 # response_msg tpl
 TextTpl = '''<xml>
