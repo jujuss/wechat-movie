1c34cb9f5363575e64e792f03848a3ef63800a36
steven-hl <steven.hl.0901@gmail.com>
2015-03-15 03:35:57 +0800

    add crawler cron

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
-python index.py 1>>wechat-movie.log 2>>wechat-movie.log &
+pip install -r requirements.txt
+python index.py >> wechat-movie.log 2>&1 &
+python tools/crawler.py >> crawler.log 2>&1 &
+
+BASEDIR=`dirname $0`
+BASEDIR=`(cd "$BASEDIR"; pwd)`
+CRONTAB_CMD="* */12 * * * python $BASEDIR/tools/crawler.py > $BASEDIR/crawler.log 2>&1 &"
+(crontab -l 2>/dev/null | echo "$CRONTAB_CMD") | crontab -
+COUNT=`crontab -l |  grep -v "grep"|wc -l `
+if [ $COUNT -lt 1 ]; then
+        echo "fail to add crontab $PROGRAM"
+        exit 1
+fi
