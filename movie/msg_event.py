fe3f590c1130284a63d1d4ca91aa9fd66aa16ff7
steven-hl <steven.hl.0901@gmail.com>
2015-03-17 00:58:01 +0800

    获取附近影院信息

--- movie/msg_event.py
+++ movie/msg_event.py
@@ -7,6 +7,7 @@ import json
 
 import config
 from libs import mcurl
+from . import app
 
 class EventMsg(object):
     def __init__(self,msg):
@@ -92,9 +93,9 @@ class EventMsg(object):
             baidu_map_long = baidu_location['x']
             baidu_map_lat = baidu_location['y']
 
-            place_search_url = '%s?ak=%s&query=%s&location=%s&radius=%s&output=%s' % (config.baidu_map_place_api, config.baidu_ak, '电影院', '%s,%s' % (baidu_map_lat, baidu_map_long), '3000', 'json')
+            place_search_url = '%s?ak=%s&query=%s&location=%s&radius=%s&output=%s&scope=%s' % (config.baidu_map_place_api, config.baidu_ak, '电影院', '%s,%s' % (baidu_map_lat, baidu_map_long), '3000', 'json', '2')
             search_result = json.loads(mcurl.CurlHelper().get(place_search_url))
-            print search_result
+            app.logger.info(search_result) # 电影院信息
 
             return ('经度: %s, 纬度: %s' % (baidu_map_long, baidu_map_lat, ),'text')
         else:
