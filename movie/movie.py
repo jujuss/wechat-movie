6b9c25c6a41ee3fb10422408a117c8ab86ca9e2b
steven.hl <steven.hl.0901@gmail.com>
2015-03-19 23:02:39 +0800

    fix bug

--- movie/movie.py
+++ movie/movie.py
@@ -1,11 +1,12 @@
 # coding:utf-8
 
-from flask import make_response,request, redirect
+from flask import make_response, request, redirect
 from . import app
 
 from libs import mcurl
 
+@app.route('/movie/subject/<douban_id>/',methods=['GET','POST'])
 @app.route('/movie/subject/<douban_id>',methods=['GET','POST'])
 def show_douban(douban_id):
     # return mcurl.CurlHelper(mobile=True).get('http://movie.douban.com/subject/%s/'  % douban_id.encode('utf-8'))
-    return redirect('http://movie.douban.com/subject/%s/' % douban_id.encode('utf-8'))
+    return redirect('http://movie.douban.com/subject/%s/' %  douban_id.encode('utf-8'))
