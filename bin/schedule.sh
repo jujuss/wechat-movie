7c60b62a08b776ebdda5da2b0025f0e808d7cc9b
steven-hl <steven.hl.0901@gmail.com>
2015-04-19 00:12:13 +0800

    update cron

--- bin/schedule.sh
+++ bin/schedule.sh
@@ -1,9 +1,17 @@
 BASEDIR=`dirname $0`
-BASEDIR=`(cd "$BASEDIR"; pwd)`
-CRONTAB_CMD="* */12 * * * python $BASEDIR/bin/spider.py > $BASEDIR/spider.log 2>&1 &"
-(crontab -l 2>/dev/null | echo "$CRONTAB_CMD") | crontab -
-COUNT=`crontab -l |  grep -v "grep"|wc -l `
-if [ $COUNT -lt 1 ]; then
-        echo "fail to add crontab $PROGRAM"
-        exit 1
-fi
+BASEDIR=`(cd "$BASEDIR/.."; pwd)`
+
+# spider
+CRONTAB_CMD="30 0 * * * python $BASEDIR/bin/spider.py > $BASEDIR/spider.log 2>&1 &"
+echo "$CRONTAB_CMD" | crontab -
+CRONTAB_CMD="0 8 * * * python $BASEDIR/bin/spider.py > $BASEDIR/spider.log 2>&1 &"
+echo "$CRONTAB_CMD" | crontab -
+
+#upload media
+CRONTAB_CMD="0 16 * * * python $BASEDIR/bin/media.py > $BASEDIR/upload_media.log 2>&1 &"
+echo "$CRONTAB_CMD" | crontab -
+
+
+#send msg
+CRONTAB_CMD="30 17 * * * python $BASEDIR/bin/msg.py > $BASEDIR/push_msg.log 2>&1 &"
+echo "$CRONTAB_CMD" | crontab -
