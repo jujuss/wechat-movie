4b23fcd06e33f8f9cd36be1e1be7eacd98411ad9
steven-hl <steven.hl.0901@gmail.com>
2015-04-19 17:35:05 +0800

    重定向豆瓣的手机显示页面

--- movie/movie/show.py
+++ movie/movie/show.py
@@ -7,5 +7,5 @@ from .. import app
 @app.route('/movie/subject/<douban_id>/', methods=['GET', 'POST'])
 @app.route('/movie/subject/<douban_id>', methods=['GET', 'POST'])
 def show_douban(douban_id):
-    return redirect('http://movie.douban.com/subject/%s/' %
+    return redirect('http://movie.douban.com/subject/%s/mobile' %
                     douban_id.encode('utf-8'))
