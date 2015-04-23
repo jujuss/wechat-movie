cfd35541fc222e5ee845b6a5a81a44d361a12f31
steven-hl <steven.hl.0901@gmail.com>
2015-04-23 22:51:40 +0800

    修改python为打包后的python环境

--- bin/schedule.sh
+++ bin/schedule.sh
@@ -3,18 +3,20 @@ BASEDIR=`(cd "$BASEDIR/.."; pwd)`
 
 crontab -l > ~/.crontab
 
+PYTHON="/data/virtualenv/wechat-movie/bin/python"
+
 # spider
-CRONTAB_CMD="30 0 * * * python $BASEDIR/bin/spider.py >> $BASEDIR/spider.log 2>&1 &"
+CRONTAB_CMD="30 0 * * * $PYTHON $BASEDIR/bin/spider.py >> $BASEDIR/spider.log 2>&1 &"
 echo "$CRONTAB_CMD" >> ~/.crontab
-CRONTAB_CMD="0 8 * * * python $BASEDIR/bin/spider.py >> $BASEDIR/spider.log 2>&1 &"
+CRONTAB_CMD="0 8 * * * $PYTHON $BASEDIR/bin/spider.py >> $BASEDIR/spider.log 2>&1 &"
 echo "$CRONTAB_CMD" >> ~/.crontab
 
 #upload media
-CRONTAB_CMD="0 16 * * * python $BASEDIR/bin/media.py >> $BASEDIR/upload_media.log 2>&1 &"
+CRONTAB_CMD="0 16 * * * $PYTHON $BASEDIR/bin/media.py >> $BASEDIR/upload_media.log 2>&1 &"
 echo "$CRONTAB_CMD" >> ~/.crontab
 
 
 #send msg
-CRONTAB_CMD="30 17 * * * python $BASEDIR/bin/msg.py >> $BASEDIR/push_msg.log 2>&1 &"
+CRONTAB_CMD="30 17 * * * $PYTHON $BASEDIR/bin/msg.py >> $BASEDIR/push_msg.log 2>&1 &"
 echo "$CRONTAB_CMD" >> ~/.crontab
 
