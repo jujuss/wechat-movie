681713b094019885a499a38e0260ef8bdcdc2b94
steven-hl <steven.hl.0901@gmail.com>
2015-04-23 22:56:27 +0800

    change to virtual python

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,3 @@
-python bin/index.py >> wechat-movie.log 2>&1 &
-python bin/spider.py >> spider.log 2>&1 &
+/data/virtualenv/wechat-movie/bin/python bin/index.py >> wechat-movie.log 2>&1 &
+/data/virtualenv/wechat-movie/bin/python bin/spider.py >> spider.log 2>&1 &
 
